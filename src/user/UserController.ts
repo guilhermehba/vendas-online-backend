e900f8b758491f36f613e373497b9c4f0b58765f
guilhermehba <guilhermemalka6@gmail.com>
2024-10-07 12:12:31 -0300

    integração com banco de dados postgreSQL mock

--- src/user/UserController.ts
+++ src/user/UserController.ts
@@ -1,19 +1,20 @@
 import { CreateUserDto } from './dtos/createUser.dto';
 import { Body, Controller, Get, Post } from "@nestjs/common";
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
 
 
 @Controller('user')
 export class UserController {
-  @Get()
-  async getAllUsers() {
-   return JSON.stringify({ test: 'abc' });
-  }
+  
+  constructor(private userService: UserService) {}
 
   @Post()
   async createUser(@Body() createUser:CreateUserDto) {
-   return {
-    ...createUser,
-    password:undefined
-   }
+    return this.userService.createUser(createUser)
+  }
+  @Get()
+  async getAllUsers():Promise<UserEntity[]> {
+   return this.userService.getAllUser()
   }
 }
